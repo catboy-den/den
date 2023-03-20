395645a9751262303bba04013d4b647eab4d28a2
Twinki14 <thetwinkehh@gmail.com>
2023-03-19 23:30:05 -0400

    try some more

--- cluster/secrets/vault-secrets/main.tf
+++ cluster/secrets/vault-secrets/main.tf
@@ -1,3 +1,24 @@
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    vault = {
+      source  = "hashicorp/vault"
+      version = "~> 2.22"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.4.1"
+    }
+  }
+}
+
 output "vault_namespace" {
   value = var.vault_namespace
 }
+
+/* 
+provider "vault" {
+  address   = local.vault_addr
+  token     = local.vault_token
+  namespace = local.vault_namespace
+} */

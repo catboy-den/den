233b7f3d33e46cbea96499c4050ba51a23d0ef44
Twinki14 <thetwinkehh@gmail.com>
2023-03-23 01:43:51 -0400

    okay maybe we need the token

--- cluster/secrets/vault-secrets/main.tf
+++ cluster/secrets/vault-secrets/main.tf
@@ -14,12 +14,13 @@ terraform {
 
 provider "vault" {
   address = "https://vault:8200"
+  token   = var.vault-root
 }
 
 resource "vault_generic_secret" "example_secret" {
   path = "secret/example"
   data_json = jsonencode({
-    username = "myuser--evenasdsadasds"
+    username = "myuser-asdsadasdsad"
     password = "mypassword"
   })
 }
